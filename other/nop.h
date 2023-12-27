d7b51ba145e7f6fcb68c29fccfa827b55e8816ba
misnikan <misnik.as@phystech.edu>
2023-12-27 20:34:35 +0300

    V3 ready

--- other/nop.h
+++ other/nop.h
@@ -6,14 +6,16 @@ ALL IN LONG
 
 nop(vector<long long> s1, vector<long long> s2) - construct & calculate
 nop.ans - return length of longest common subsequence of given vectors (after construction)
+nop.pos - return vector of indexes of elements of longest common subsequence
 */
 
 struct nop {
     long long ans = 0;
-    vector <pair<int, int>> pos; // TODO: add positions calculation
+    vector <pair<int, int>> pos;
 
     vector <long long> a;
     vector <long long> b;
+    bool sw = false;
 
 
     nop(vector<long long>& s1, vector <long long>& s2) {
@@ -21,6 +23,7 @@ struct nop {
         b = s2;
         if (a.size() > b.size()) {
             swap(a, b);
+            sw = true;
         }
         calc();
     }
@@ -30,6 +33,7 @@ struct nop {
         int m = b.size();
         vector<long long> prev(n + 1, 0LL);
         vector<long long> cur(n + 1, 0LL);
+        vector <long long> last(n + 1, 0LL);
 
         for (int idx2 = 0; idx2 < n + 1; idx2++) {
             cur[idx2] = 0;
@@ -39,6 +43,7 @@ struct nop {
             for (int idx2 = 1; idx2 < n + 1; idx2++) {
                 if (b[idx1 - 1] == a[idx2 - 1]) {
                     cur[idx2] = 1 + prev[idx2 - 1];
+                    last[idx2] = idx1;
                 }
                 else
                     cur[idx2] = max(cur[idx2 - 1], prev[idx2]);
@@ -46,5 +51,17 @@ struct nop {
             prev = cur;
         }
         ans = cur[n];
+        int idx = ans;
+        for (int i = n; i >= 1; --i) {
+            if (cur[i] == idx) {
+                idx--;
+                if (sw) {
+                    pos.push_back({last[i] - 1, i - 1});
+                } else {
+                    pos.push_back({i - 1, last[i] - 1});
+                }
+            }
+        }
+        reverse(pos.begin(), pos.end());
     }
 };
